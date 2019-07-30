82f04ef853fbb55c04584e5c5733b11c1cf1a98f
MJ Linane <mj@mjlinane.com>
2019-07-30 16:17:01 -0400

    Add gameOver function

--- 3 DOM Projects/Project 3 Number Guesser/app.js
+++ 3 DOM Projects/Project 3 Number Guesser/app.js
@@ -1,3 +1,8 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-use-before-define */
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
@@ -18,6 +23,8 @@ const max = 5;
 const winningNum = getRandomInt(max);
 let guessesLeft = 3;
 
+console.log(winningNum);
+
 // UI Elements
 const UIgame = document.querySelector('#game');
 const UIminNum = document.querySelector('.min-num');
@@ -36,60 +43,71 @@ UIguessButton.addEventListener('click', function () {
 
   // Validate to make sure it goes in between the min and max
   // Use function isNaN() to check if it is a not a number
-  // eslint-disable-next-line no-restricted-globals
   if (isNaN(guess) || guess < min || guess > max) {
     // Add warning text and form
     UImessage.classList.add('uk-text-danger');
     UIguessInput.classList.add('uk-form-danger');
-    UIguessInput.classList.add('uk-animation-shake');
     setMessage(`Please enter a number between ${min} and ${max}`);
-    UIguessInput.classList.remove('uk-animation-shake');
 
-    // Check guess if correct
+
+  // Check guess if correct
   } else if (guess === winningNum) {
     // Game over - won
-    // Disable input
-    UIguessInput.disabled = true;
-    // Remove red text
-    UImessage.classList.remove('uk-text-danger');
-    // Add green text
-    UImessage.classList.add('uk-text-success');
-    setMessage(`${winningNum} is correct, YOU WIN!`);
+    gameOver(true, `${guess} is correct, YOU WIN!`);
   } else {
     // Wrong number, subtract 1
     guessesLeft -= 1;
-
     if (guessesLeft === 0) {
       // Game over - lost
-      // Disable input
-      UIguessInput.disabled = true;
-      // Add red text & form
-      UImessage.classList.add('uk-text-danger');
-      UIguessInput.classList.add('uk-text-danger');
-      UIguessInput.classList.add('uk-animation-shake');
-      setMessage(`You have lost, the correct number was ${winningNum}`);
-      UIguessInput.classList.remove('uk-animation-shake');
+      gameOver(false, `You have lost, the correct number was ${winningNum}`);
     } else {
       // Game continues - answer wrong
       // Add warning text color
+      UImessage.classList.remove('uk-text-success');
       UImessage.classList.add('uk-text-danger');
       // Add warning text color
+      UIguessInput.classList.remove('uk-form-success');
       UIguessInput.classList.add('uk-form-danger');
-      // Add warning shake
-      UIguessInput.classList.add('uk-animation-shake');
+
       // Clear input
       UIguessInput.value = '';
+      // Tell user its the wront number
       setMessage(
-        `${guess} was not correct, you have ${guessesLeft} guesses remaining.`,
+        `${guess} was not correct, you have ${guessesLeft} guesses remaining.`
       );
-      UIguessInput.classList.remove('uk-animation-shake');
     }
   }
 });
 
+// Game Over
+function gameOver(won, message) {
+  if (won === true) {
+    // Add green text color
+    UImessage.classList.remove('uk-text-danger');
+    UImessage.classList.add('uk-text-success');
+    // Add green form color
+    UIguessInput.classList.remove('uk-form-danger');
+    UIguessInput.classList.add('uk-form-success');
+  } else {
+    // Add warning text color
+    UImessage.classList.remove('uk-text-success');
+    UImessage.classList.add('uk-text-danger');
+    // Add warning text color
+    UIguessInput.classList.remove('uk-form-success');
+    UIguessInput.classList.add('uk-form-danger');
+  }
+  // Disable input
+  UIguessInput.disabled = true;
+
+  // Display message to user
+  setMessage(message);
+
+  // Set submit button to 'play again'
+}
+
 // Set Message
-function setMessage(message) {
-  UImessage.textContent = message;
+function setMessage(displayMessage) {
+  UImessage.textContent = displayMessage;
 }
 
 // Get random integer
