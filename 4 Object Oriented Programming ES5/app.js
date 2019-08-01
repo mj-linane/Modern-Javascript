804f0ab6a75021f14f13811e39d77ec0a08a3cad
MJ Linane <mj@mjlinane.com>
2019-08-01 14:21:05 -0400

    cleared

--- 4 Object Oriented Programming ES5/app.js
+++ 4 Object Oriented Programming ES5/app.js
@@ -1,14 +1,1 @@
-class Person {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthday = new Date(dob);
-  }
 
-  greeting() {
-    return `Hello there ${this.firstName} ${this.lastName} you were born on ${this.birthday}`;
-  }
-}
-
-const mary = new Person('Mary', 'Williams', '11-13-1980');
-console.log(mary.greeting());
