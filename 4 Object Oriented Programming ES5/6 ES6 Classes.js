be3b60bb3b5d46bc894bb0b223b6518e1f074474
MJ Linane <mj@mjlinane.com>
2019-08-01 14:20:49 -0400

    Initial

--- 4 Object Oriented Programming ES5/6 ES6 Classes.js
+++ 4 Object Oriented Programming ES5/6 ES6 Classes.js
@@ -1,20 +1,38 @@
-/* eslint-disable object-shorthand */
-/* eslint-disable no-new-object */
-/* eslint-disable no-array-constructor */
-/* eslint-disable no-new-func */
-/* eslint-disable func-names */
-/* eslint-disable no-new-wrappers */
-
 class Person {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.birthday = new Date(dob);
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  // Static method can be used without instantiating a new object
+  static addNumbers(x, y) {
+    return x + y;
+  }
 }
 
-const mary = new Person('Mary', 'Williams');
-console.log(mary.greeting);
+const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+mary.getsMarried('Thompson');
+
+console.log(mary);
+
+// Calling a static method -- Incorrect-- this will throw an error
+// console.log(mary.addNumbers(2, 3));
+
+// Calling a static method -- Correct --
+console.log(Person.addNumbers(2, 3));
