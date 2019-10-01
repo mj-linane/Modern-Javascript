e32db434345fc785e5af65d0c2f263d5326aa2aa
MJ Linane <mj@mjlinane.com>
2019-10-01 07:04:58 -0400

    Initial

--- 1 Javascript Fundamentals/11 Javascript Template Literals-Template Strings.js
+++ 1 Javascript Fundamentals/11 Javascript Template Literals-Template Strings.js
@@ -1,27 +1,23 @@
+/* eslint-disable no-useless-concat */
 const name = 'MJ';
 const age = 30;
 const job = 'CS Teacher';
 const state = 'Massachusetts';
 let html;
 
-
 // Without template strings (es5)
-html = '<ul>' +
-    '<li>Name: ' + name + '</li>' +
-    '<li>Age: ' + age + '</li>' +
-    '<li>Job: ' + job + '</li >' +
-    '<li>State: ' + state + '</li>' +
-    '</ul >'
-;
-
-/**
- * Returns hello
- * @return {string}
- */
-function hello() {
-  return 'hello';
-}
-
+html = `${`${'<ul>' + '<li>Name: '}${name}</li>` +
+  `<li>Age: ${age}</li>` +
+  `<li>Job: ${job}</li >` +
+  `<li>State: ${state}</li>`}${
+  /**
+   * Returns hello
+   * @return {string}
+   */
+  function hello() {
+    return 'hello';
+  }
+}`;
 
 // With template strings (es6)
 html = `
@@ -32,9 +28,8 @@ html = `
         <li>State: ${state}</li>
         <li>${2 + 2}</li>
         <li>${hello()}</li>
-        <li>${age>31 ? 'Over 30' : 'Under 30'}</li>
+        <li>${age > 31 ? 'Over 30' : 'Under 30'}</li>
     </ul >
 `;
-
 
 document.body.innerHTML = html;
---
+++ 6 Asynchronous AJAX and Fetch API/7 Fetch API/app.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-use-before-define */
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getApi);
+
+// Get local text file data
+function getText() {
+  // fetch responds with a promise
+  fetch('test.txt')
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      console.log(data);
+      document.getElementbyID('output').innerHTML = data;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+// Get local JSON data
+function getJson() {
+  // fetch responds with a promise
+  fetch('posts.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+// Get API data
+function getApi() {
+  // fetch responds with a promise
+  fetch('https://api.github.com/users')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
